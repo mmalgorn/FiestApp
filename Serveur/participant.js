def0224ae98f518396ab58e7b540e2a70062b2ef
malm1803 <mathieu.malgorn@usherbrooke.ca>
2017-12-02 19:06:07 -0500

    init participant

---
+++ Serveur/participant.js
@@ -0,0 +1,51 @@
+"use strict";
+
+console.log("test");
+
+class Participant{
+
+  constructor(id){
+    this.id = id;
+    this.position = 'pos';
+    this.nom = '';
+    this.prenom = '';
+
+  }
+
+  set nom(name){
+    this._nom = name.charAt(0).toUpperCase() + name.slice(1);
+  }
+  set prenom(name){
+    this._prenom = name.charAt(0).toUpperCase() + name.slice(1);
+  }
+  set position(pos){
+    this._position = pos;
+  }
+  set id(id){
+    this._id = id;
+  }
+  get id(){
+    return this._id;
+  }
+  get nom(){
+    return this._nom;
+  }
+  get prenom(){
+    return this._prenom;
+  }
+  get position(){
+    return this._position;
+  }
+
+  test(){
+    console.log('Nom '+this.nom+' Prenom '+ this.prenom+' ID '+this.id);
+  }
+
+}
+console.log("coucou");
+var testParticipant = new Participant('testid');
+console.log("coucou");
+testParticipant.test();
+testParticipant.nom = 'Malgorn';
+testParticipant.prenom = 'Mathieu';
+testParticipant.test();
