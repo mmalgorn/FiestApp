556dfb187fa165cc24e2edd2efcc4af2693483f2
malm1803 <mathieu.malgorn@usherbrooke.ca>
2017-12-12 18:51:19 -0500

    co fb

--- FiestApp/app/src/main/java/com/example/mathieu/fiestapp/MainActivity.java
+++ FiestApp/app/src/main/java/com/example/mathieu/fiestapp/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.mathieu.fiestapp;
 
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.Profile;
 import com.facebook.appevents.AppEventsLogger;
 
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -18,12 +34,20 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Signature;
+import java.util.concurrent.Callable;
+
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
-
+    private CallbackManager callbackManager;
+    public static final String TAG = "MainActivity";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,12 +61,93 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             mainFragment = (MainFragment) getSupportFragmentManager()
                     .findFragmentById(android.R.id.content);
         }*/
-
         setContentView(R.layout.activity_main);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+       /* try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.mathieu.fiestapp",
+                    PackageManager.GET_SIGNATURES);
+            for (android.content.pm.Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }*/
+
+
+        //Facebook
+
+        // Initialize your instance of callbackManager//
+        callbackManager = CallbackManager.Factory.create();
+        Log.d(TAG,"BeforeLogin");
+
+        // Register your callback//
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+
+                // If the login attempt is successful, then call onSuccess and pass the LoginResult//
+                new FacebookCallback<LoginResult>() {
+
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // Print the user’s ID and the Auth Token to Android Studio’s Logcat Monitor//
+                        Log.d(TAG, "User ID: " +
+                                loginResult.getAccessToken().getUserId() + "\n" +
+                                "Auth Token: " + loginResult.getAccessToken().getToken());
+                    }
+
+                    // If the user cancels the login, then call onCancel//
+                    @Override
+                    public void onCancel() {
+                        Log.d(TAG,"Cancel");
+
+                    }
+
+                    // If an error occurs, then call onError//
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Log.d(TAG,exception.toString());
+                    }
+                });
+        Log.d(TAG,"BeforeLogin"+ AccessToken.getCurrentAccessToken());
+        if(Profile.getCurrentProfile()!=null) {
+            Log.d(TAG, Profile.getCurrentProfile().getFirstName());
+
+        }
+        new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/"+Profile.getCurrentProfile().getId()+"/taggable_friends?limit=500",
+                null,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+            /* handle the result */
+                if(response!=null) {
+                    Log.d(TAG, response.getJSONObject().toString());
+                    try {
+                        JSONArray amis = response.getJSONObject().getJSONArray("data");
+                        Log.d(TAG,"nb Amis"+amis.length());
+                        for(int i =0;i<amis.length();i++){
+                            Log.d(TAG,amis.get(i).toString() );
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else Log.d(TAG,"No response");
+                    }
+                }
+        ).executeAsync();
     }
 
     @Override
@@ -54,5 +159,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap.addMarker(new MarkerOptions().position(sydney).title("UN PD VIT ICI ET NOUS FAIT A BOUFFER TOUS LES JOURS"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
