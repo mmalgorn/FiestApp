3391e65e325c1a6f9a9b5088b09f27862d7aa697
malm1803 <mathieu.malgorn@usherbrooke.ca>
2017-12-15 11:58:27 -0500

    Google place

--- FiestApp/app/src/main/java/andoird/fiestapp/ActivityReglages.java
+++ FiestApp/app/src/main/java/andoird/fiestapp/ActivityReglages.java
@@ -3,6 +3,7 @@ package andoird.fiestapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,14 +12,40 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+
 import andoird.fiestapp.Object.Soiree;
 
 public class ActivityReglages extends AppCompatActivity {
+
+    private static final String TAG = "ACTIVITY REGLAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reglages);
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName());
+                place.getLatLng();
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
 
         Button textDatedebut =  (Button)findViewById(R.id.creation_invitation);
         textDatedebut.setOnClickListener(new View.OnClickListener() {
