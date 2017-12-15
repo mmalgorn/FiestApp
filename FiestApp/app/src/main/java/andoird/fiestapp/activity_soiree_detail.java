028c589be52eb5527bfea8f9edfe9cb187246df5
livs2801 <sebliv@hotmail.fr>
2017-12-15 12:36:11 -0500

    Affichage des cartes de soirees

--- FiestApp/app/src/main/java/andoird/fiestapp/activity_soiree_detail.java
+++ FiestApp/app/src/main/java/andoird/fiestapp/activity_soiree_detail.java
@@ -1,13 +1,8 @@
 package andoird.fiestapp;
 
 import android.content.Intent;
-import android.nfc.Tag;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -24,11 +19,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 
 import andoird.fiestapp.Object.MapMarqueurUser;
-import andoird.fiestapp.Object.Soiree;
 import andoird.fiestapp.Object.User;
 import andoird.fiestapp.rest.Rest;
 
@@ -81,84 +74,57 @@ public class activity_soiree_detail extends AppCompatActivity implements OnMapRe
         mMap = googleMap;
         MyApplication app = (MyApplication) getApplicationContext();
 
-        LatLng positionSoiree = new LatLng(app.laSoiree.getPosition()[0],app.laSoiree.getPosition()[1]);
+        LatLng positionSoiree = new LatLng(app.laSoiree.getPosition()[0], app.laSoiree.getPosition()[1]);
 
         mMap.addMarker(new MarkerOptions()
                 .position(positionSoiree).title("Soiree")
                 .zIndex(1.0f)
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.logo_fiestapp))
         );
-//        LatLng positionUser = new LatLng(app.laSoiree.getPosition()[0]+1,app.laSoiree.getPosition()[1]+2);
-//        mMap.addMarker(new MarkerOptions()
-//                .position(positionUser)
-//                .title("Marqueur ")
-//                .snippet("statut ")
-//                .icon(BitmapDescriptorFactory.defaultMarker(60 ))
-//                .zIndex(2.0f)
-//        );
-//        Log.d("TAG", String.valueOf(app.laSoiree.getParticipants().size()));
-//
-//        for(int i=0; i<10; i++){
-//            LatLng positionUser2 = new LatLng(app.laSoiree.getPosition()[0]+i,app.laSoiree.getPosition()[1]+i);
-//            mMap.addMarker(new MarkerOptions()
-//                                .position(positionUser2)
-//                                .title("Marqueur "+i)
-//                                .snippet("statut "+i)
-//                            .icon(BitmapDescriptorFactory.defaultMarker(12*i%360))
-//            );
-//        }
-//
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(positionSoiree));
 
-//        Rest rest = null;
-//        try {
-//            rest = new Rest();
-//            JSONObject obj1 = new JSONObject();
-//
-//
-//            Soiree s = (Soiree) rest.execute("/GetSoiree", obj1).get();
-//            if (s != null) {
-//                int length = s.getParticipants().size();
-//
-//                for(int i=0; i< length; i++){
-//                    rest = new Rest();
-//                    User u = (User) rest.execute("/FindUserById", s.getParticipants().get(i).getId()).get();
-//                    MapMarqueurUser monMarqueur = new MapMarqueurUser(u.getPosition(),u.getPrenom(),s.getParticipants().get(i).getStatus());
-//                    LatLng positionUser2 = new LatLng(monMarqueur.getPosition()[0], monMarqueur.getPosition()[1]);
-//                    if(s.getIdCreateur().equals(u.getId())){
-//                        mMap.addMarker(new MarkerOptions()
-//                                .position(positionUser2)
-//                                .title(monMarqueur.getPrenom())
-//                                .snippet(monMarqueur.getStatut())
-//                                .flat(true)
-//                                .zIndex(2.0f)
-//                        );
-//
-//                        mMap.moveCamera(CameraUpdateFactory.newLatLng(positionUser));
-//                    }
-//                    else{
-//                        mMap.addMarker(new MarkerOptions()
-//                                .position(positionUser)
-//                                .title(monMarqueur.getPrenom())
-//                                .snippet(monMarqueur.getStatut())
-//                                .icon(BitmapDescriptorFactory.defaultMarker(12 * i % 360))
-//                        );
-//
-//                    }
-//                }
-//            } else {
-//                int[] posS = new int[2];
-//                s = new Soiree("5a3317814c18cb19cde88c84", 78, 78, "TestSoire", posS);
-//            }
-//        }
-//        catch(InterruptedException e){
-//            e.printStackTrace();
-//        } catch (ExecutionException e) {
-//            e.printStackTrace();
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
-    }
+        Rest rest = null;
+        try {
+            rest = new Rest();
+
+
+
+
+            int length = app.laSoiree.getParticipants().size();
+
+            for (int i = 0; i < length; i++) {
+                rest = new Rest();
+                JSONObject obj1 = new JSONObject();
+                obj1.put("id",app.laSoiree.getParticipants().get(i).getId());
+                User u = (User) rest.execute("/FindUserById", obj1).get();
+                MapMarqueurUser monMarqueur = new MapMarqueurUser(u.getPosition(), u.getPrenom(), app.laSoiree.getParticipants().get(i).getStatus());
+                LatLng positionUser2 = new LatLng(monMarqueur.getPosition()[0], monMarqueur.getPosition()[1]);
+                if (app.getMyUser().getId().equals(u.getId())) {
+                    mMap.addMarker(new MarkerOptions()
+                            .position(positionUser2)
+                            .title(monMarqueur.getPrenom())
+                            .snippet(monMarqueur.getStatut())
+                            .flat(true)
+                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.cursor_user))
+                            .zIndex(2.0f)
+                    );
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(positionUser2));
+                } else {
+                    mMap.addMarker(new MarkerOptions()
+                            .position(positionUser2)
+                            .title(monMarqueur.getPrenom())
+                            .snippet(monMarqueur.getStatut())
+                            .icon(BitmapDescriptorFactory.defaultMarker(60 * i % 360))
+                    );
+                }
+            }
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    };
 
 
 //    @Override
--- FiestApp/app/src/main/java/andoird/fiestapp/rest/Rest.java
+++ FiestApp/app/src/main/java/andoird/fiestapp/rest/Rest.java
@@ -50,8 +50,8 @@ public class Rest extends AsyncTask {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.FROYO) {
             System.setProperty("http.keepAlive", "false");
         }
-//        this.path="http://10.0.2.2:3000";
-        this.path = "http://192.168.196.1:3000";
+        this.path="http://10.0.2.2:3000";
+//        this.path = "http://192.168.196.1:3000";
         Object retour = null;
         switch (objects[0].toString()){
             case "/FindUser":
